a052cbfbf4736f242f93e293299fe48e2e6ed99d
william <452522426@qq.com>
2024-08-09 11:19:05 +0800

    1.0.0

--- app/src/main/java/com/mrk/demo/App.java
+++ app/src/main/java/com/mrk/demo/App.java
@@ -6,9 +6,10 @@ import com.mrk.device.MrkDeviceManger;
 
 
 public class App extends Application {
+    String sign = "Ov92oVQaZB4d8XIT2I9Bhd0/sg6YxBkJC+AyGSSjrQE1vT9286tiroJslO4eYTZLiebxKNQVWIw03dxPVOO76FcGZJJ2MHSqJvHgRRk2w+dIcjisz1gUiEEUktswF81H+ezDc93rd8c802zONh3ResNMdxUveKNcEMdlNh6M5vMzMRFT94KlUWtiNpPR48jHLIQsjwzpmTcLWqyAy5Gvqw==";
     @Override
     public void onCreate() {
         super.onCreate();
-        MrkDeviceManger.INSTANCE.init(this,true);
+        MrkDeviceManger.INSTANCE.init(this,sign,true);
     }
 }
